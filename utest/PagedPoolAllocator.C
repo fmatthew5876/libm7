7800972107e4a79e825b60e0ac90bfcc07f74a1d
Matthew Fioravante <fmatthew5876@gmail.com>
2017-11-08 19:01:46 -0500

    Add PagedPoolAllocator

---
+++ utest/PagedPoolAllocator.C
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <m7/PagedPoolAllocator.H>
+
+using namespace m7;
+
+template <typename T, size_t N>
+void test_alloc(int nobjs_per_page) {
+
+    auto p = PagedPoolAllocator<T>(nobjs_per_page);
+
+    std::array<T*, N> ptrs;
+
+    auto alloc_fwd = [&p](auto& ptrs) {
+        for(auto& ptr: ptrs) {
+            ptr = p.alloc();
+        }
+    };
+
+    alloc_fwd(ptrs);
+
+    for(auto& ptr: ptrs) {
+        p.free(ptr);
+    }
+
+    alloc_fwd(ptrs);
+
+    for(auto iter = ptrs.rbegin(); iter != ptrs.rend(); ++iter) {
+        p.free(*iter);
+    }
+}
+
+TEST(PagedPoolAllocator, test) {
+    test_alloc<int,31>(4);
+    test_alloc<int,259>(500);
+    test_alloc<int,147>(288);
+    test_alloc<int,100>(50);
+    test_alloc<int,7>(1);
+}
