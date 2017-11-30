8857d5e834c219c780fcf8be9fc4e90db4448895
Matthew Fioravante <fmatthew5876@gmail.com>
2017-11-30 05:57:01 -0500

    Add more useful methods to ArrayView

--- utest/ArrayView.C
+++ utest/ArrayView.C
@@ -175,6 +175,14 @@ TEST(ArrayView, prefix) {
     ASSERT_EQ(v.size(), 5u);
     ASSERT_FALSE(v.empty());
 
+    auto v2 = remove_prefix(v, 2);
+    ASSERT_EQ(v2.size(), 3u);
+    ASSERT_FALSE(v2.empty());
+
+    ASSERT_EQ(v2[0], 3);
+    ASSERT_EQ(v2[1], 4);
+    ASSERT_EQ(v2[2], 5);
+
     v.remove_prefix(2);
     ASSERT_EQ(v.size(), 3u);
     ASSERT_FALSE(v.empty());
@@ -187,6 +195,35 @@ TEST(ArrayView, prefix) {
 
     ASSERT_EQ(v.size(), 0u);
     ASSERT_TRUE(v.empty());
+
+    v = ArrayView(a);
+
+    v2 = remove_prefix_trunc(v, 3);
+
+    ASSERT_EQ(v2.size(), 2u);
+    ASSERT_EQ(v2[0], 4);
+    ASSERT_EQ(v2[1], 5);
+
+    v2 = remove_prefix_trunc(v, 4);
+
+    ASSERT_EQ(v2.size(), 1u);
+    ASSERT_EQ(v2[0], 5);
+
+    v2 = remove_prefix_trunc(v, 5);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = remove_prefix_trunc(v, 6);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = remove_prefix_trunc(v, 9999);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
 }
 
 TEST(ArrayView, suffix) {
@@ -196,6 +233,15 @@ TEST(ArrayView, suffix) {
 
     ASSERT_EQ(v.size(), 5u);
     ASSERT_FALSE(v.empty());
+
+    auto v2 = remove_suffix(v, 2);
+    ASSERT_EQ(v2.size(), 3u);
+    ASSERT_FALSE(v2.empty());
+
+    ASSERT_EQ(v2[0], 1);
+    ASSERT_EQ(v2[1], 2);
+    ASSERT_EQ(v2[2], 3);
+
 
     v.remove_suffix(2);
     ASSERT_EQ(v.size(), 3u);
@@ -209,6 +255,228 @@ TEST(ArrayView, suffix) {
 
     ASSERT_EQ(v.size(), 0u);
     ASSERT_TRUE(v.empty());
+
+    v = ArrayView(a);
+
+    v2 = remove_suffix_trunc(v, 3);
+
+    ASSERT_EQ(v2.size(), 2u);
+    ASSERT_EQ(v2[0], 1);
+    ASSERT_EQ(v2[1], 2);
+
+    v2 = remove_suffix_trunc(v, 4);
+
+    ASSERT_EQ(v2.size(), 1u);
+    ASSERT_EQ(v2[0], 1);
+
+    v2 = remove_suffix_trunc(v, 5);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = remove_suffix_trunc(v, 6);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = remove_suffix_trunc(v, 9999);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+}
+
+TEST(ArrayView, slice) {
+    int a[] = { 1, 2, 3, 4, 5 };
+
+    auto v = ArrayView(a);
+
+    ASSERT_EQ(v.size(), 5u);
+    ASSERT_FALSE(v.empty());
+
+    auto v2 = slice(v, 0, 5);
+
+    ASSERT_EQ(v2.size(), v.size());
+    ASSERT_EQ(v2.front(), v.front());
+    ASSERT_EQ(v2.back(), v.back());
+
+    v2 = slice(v, 0, 4);
+
+    ASSERT_EQ(v2.size(), 4u);
+    ASSERT_EQ(v2.front(), 1);
+    ASSERT_EQ(v2.back(), 4);
+
+    v2 = slice(v, 1, 4);
+
+    ASSERT_EQ(v2.size(), 4u);
+    ASSERT_EQ(v2.front(), 2);
+    ASSERT_EQ(v2.back(), 5);
+
+    v2 = slice(v, 2, 2);
+
+    ASSERT_EQ(v2.size(), 2u);
+    ASSERT_EQ(v2.front(), 3);
+    ASSERT_EQ(v2.back(), 4);
+
+    v2 = slice(v, 2, 0);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice(v, 5, 0);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice_trunc(v, 0, 5);
+
+    ASSERT_EQ(v2.size(), v.size());
+    ASSERT_EQ(v2.front(), v.front());
+    ASSERT_EQ(v2.back(), v.back());
+
+    v2 = slice_trunc(v, 0, 4);
+
+    ASSERT_EQ(v2.size(), 4u);
+    ASSERT_EQ(v2.front(), 1);
+    ASSERT_EQ(v2.back(), 4);
+
+    v2 = slice_trunc(v, 1, 4);
+
+    ASSERT_EQ(v2.size(), 4u);
+    ASSERT_EQ(v2.front(), 2);
+    ASSERT_EQ(v2.back(), 5);
+
+    v2 = slice_trunc(v, 2, 2);
+
+    ASSERT_EQ(v2.size(), 2u);
+    ASSERT_EQ(v2.front(), 3);
+    ASSERT_EQ(v2.back(), 4);
+
+    v2 = slice_trunc(v, 2, 0);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice_trunc(v, 5, 0);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice_trunc(v, 7, 0);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice_trunc(v, 6, 4);
+
+    ASSERT_EQ(v2.size(), 0u);
+    ASSERT_TRUE(v2.empty());
+
+    v2 = slice_trunc(v, 2, 80);
+
+    ASSERT_EQ(v2.size(), 3u);
+    ASSERT_EQ(v2.front(), 3);
+    ASSERT_EQ(v2.back(), 5);
+
+    v2 = slice_trunc(v, 0, 6);
+
+    ASSERT_EQ(v2.size(), 5u);
+    ASSERT_EQ(v2.front(), 1);
+    ASSERT_EQ(v2.back(), 5);
+}
+
+TEST(ArrayView, subset) {
+    int a0[] = { 1, 2, 3, 4, 5 };
+    int a1[] = { 6, 7, 8, 9, 10 };
+
+    auto v0 = ArrayView(a0);
+    auto v1 = ArrayView(a1);
+
+    ASSERT_TRUE(is_subset(v0, v0));
+    ASSERT_TRUE(is_subset(v1, v1));
+    ASSERT_FALSE(is_subset(v0, v1));
+
+    auto v2 = remove_prefix(v0, 2);
+
+    ASSERT_TRUE(is_subset(v0, v2));
+    ASSERT_FALSE(is_subset(v1, v2));
+
+    v2 = remove_suffix(v1, 2);
+
+    ASSERT_FALSE(is_subset(v0, v2));
+    ASSERT_TRUE(is_subset(v1, v2));
+
+    v2 = ArrayView<int>();
+
+    ASSERT_FALSE(is_subset(v0, v2));
+    ASSERT_FALSE(is_subset(v1, v2));
+}
+
+TEST(ArrayView, overlap) {
+    int a0[] = { 1, 2, 3, 4, 5 };
+    int a1[] = { 6, 7, 8, 9, 10 };
+
+    auto v0 = ArrayView(a0);
+    auto v1 = ArrayView(a1);
+
+    ASSERT_TRUE(is_overlap(v0, v0));
+    ASSERT_TRUE(is_overlap(v1, v1));
+    ASSERT_FALSE(is_overlap(v0, v1));
+
+    auto v2 = remove_prefix(v0, 2);
+
+    ASSERT_TRUE(is_overlap(v0, v2));
+    ASSERT_FALSE(is_overlap(v1, v2));
+
+    v2 = remove_suffix(v1, 2);
+
+    ASSERT_FALSE(is_overlap(v0, v2));
+    ASSERT_TRUE(is_overlap(v1, v2));
+
+    v2 = ArrayView<int>();
+
+    ASSERT_FALSE(is_overlap(v0, v2));
+    ASSERT_FALSE(is_overlap(v1, v2));
+
+    v2 = slice(v0, 0, 3);
+    auto v3 = slice(v0, 2, 3);
+
+    ASSERT_TRUE(is_overlap(v2, v3));
+
+    v2.remove_suffix(1);
+
+    ASSERT_FALSE(is_overlap(v2, v3));
+
+    v3.remove_prefix(1);
+
+    ASSERT_FALSE(is_overlap(v2, v3));
+}
+
+TEST(ArrayView, same) {
+    int a0[] = { 1, 2, 3, 4, 5 };
+    int a1[] = { 6, 7, 8, 9, 10 };
+
+    auto v0 = ArrayView(a0);
+    auto v1 = ArrayView(a1);
+
+    ASSERT_TRUE(is_same(v0, v0));
+    ASSERT_TRUE(is_same(v1, v1));
+    ASSERT_FALSE(is_same(v0, v1));
+
+    auto v2 = remove_prefix(v0, 2);
+
+    ASSERT_FALSE(is_same(v0, v2));
+    ASSERT_FALSE(is_same(v1, v2));
+
+    v2 = remove_suffix(v1, 2);
+
+    ASSERT_FALSE(is_same(v0, v2));
+    ASSERT_FALSE(is_same(v1, v2));
+
+    v2 = ArrayView(a0);
+    auto v3 = ArrayView(a1);
+
+    ASSERT_TRUE(is_same(v0, v2));
+    ASSERT_TRUE(is_same(v1, v3));
 }
 
 TEST(ArrayView, iter) {
@@ -232,6 +500,8 @@ TEST(ArrayView, iter) {
     ++iter;
     ASSERT_EQ(iter, v.end());
 }
+
+
 
 TEST(ArrayView, rev_iter) {
     int a[] = { 1, 2, 3 };
